fc7fa5f7dc838c6d32f5b596e67550a508ce812b
michaelperryjnr <michaelperryt97@gmail.com>
2026-01-22 22:17:49 +0000

    created products model

---
+++ backend/products/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('<int:pk>/', views.product_detail_view),
+    path('<int:pk>/update/', views.product_update_view),
+    path('<int:pk>/delete/', views.product_destroy_view),
+    path("", views.product_list_create_view )
+]
